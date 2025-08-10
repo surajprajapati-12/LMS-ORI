53a5cfd71b7b9ae8e95f064504a47d27f0657de3
Suraj Prajapati <prajapatisuraj8779@gmail.com>
2025-08-10 22:05:34 +0530

    Initial commit — LMS MERN project

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+
+// initilize express
+const app = express()
+
+//connect to database
+await connectDB()
+
+// mdiddlewares
+
+app.use(cors())
+
+//Routes
+app.get('/', (req,res)=> res.send("API IS WORKING"))
+app.post('/clerk', express.json(), clerkWebhooks)
+
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , ()=>{
+    console.log(`server is running  on port ${PORT}`)
+})
